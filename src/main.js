24ca20e7b75e2c354e22a596f1d6960408720365
CCCCPH <2502903413@qq.com>
2023-04-17 20:31:32 +0800

    乱披风敲码

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import { Button } from 'element-ui';
+import { Input } from 'element-ui';
+import { Select } from 'element-ui';
+import { Option } from 'element-ui';
+import { Row } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
@@ -7,6 +11,13 @@ import router from './router';
 Vue.use(Button)
 Vue.config.productionTip = false
 
+Vue.use(Input)
+
+Vue.use(Select)
+
+Vue.use(Option)
+
+Vue.use(Row)
 new Vue({
   render: h => h(App),
   router
--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -1,9 +1,81 @@
 <template>
   <div>
     <h1>用户列表页面</h1>
-    <div>Hello {{ name }}，请完善相关功能</div>
+    <div v-once>Hello {{ name }}，请完善相关功能</div>
+    <!-- <p v-html="rawHtml"></p>
+    <p v-bind:class="color">111</p>
+    <p>{{ num + 1 * 10 }}</p>
+    <p>{{ a ? 'yes' : 'no' }}</p>
+    <p>{{ s.split('').reverse().join('') }}</p>
+
+    <p v-if="seen">b=true 你就能看见我 b=false 你就看不见我</p>
+
+    <div @click="click1">
+      <div @click.stop="click2">点我</div>
+    </div> -->
+
+    <!-- <p v-bind:class="[isActive ? 'active' : '', isred ? 'red' : '']"
+    style="width: 100px;height: 100px;font-size: 15px;"
+    >isActive=true 添加 isActive=false 不添加</p> -->
+
+<!-- 
+    <div 
+    :style="{color:color,fontSize:size,background:isred ? 'red' : ''}">112233</div>
+  </div> -->
+  
+  <!-- <div class="title">
+  <span>用户ID</span> <span>用户性别</span> <span>用户年龄</span>
+</div>
+
+  <div v-for="item in userList" :key="item">{{ item.id }} {{ item.sex }} {{ item.age }}</div>
+</div> -->
+
+
+  <br>
+
+  <div class="demo-input-size">
+    <el-input
+      placeholder="用户ID"
+      suffix-icon="el-icon-date"
+      v-model="input1">
+    </el-input>
+    <el-input
+      placeholder="用户性别"
+      suffix-icon="el-icon-date"
+      v-model="input2">
+    </el-input>
   </div>
+
+
+
+  <el-select v-model="value" placeholder="请选择">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select>
+
+
+    <el-button type="success" class="btn1">查询</el-button>
+    <el-button type="danger">重置</el-button>
+
+
+</div>
 </template>
+
+<!-- <template v-show="btn">
+  <div v-if="opt === 'A'">A</div>
+  <div v-else-if="opt === 'B'">B</div>
+  <div v-else-if="opt === 'C'">C</div>
+  <div v-else>not A/B/C</div>
+</template> -->
+
+<!-- 
+<template v-show="swc">
+  
+</template> -->
 
 <script>
 export default {
@@ -12,7 +84,49 @@ export default {
   components: {},
   data() {
     return {
-      name: 'cph'
+      name: 'cph',
+      // rawHtml: '<span>嗨~</span>',
+      // color: 'pink',
+      // size: '20px',
+      // s: "cph",
+      // num: 50,
+      // a: 1 + 2 > 4,
+
+      // seen: true,
+
+      // isActive: true,
+      // isred: true,
+
+      // btn: false,
+      // opt: "C",
+
+      // swc: true,
+      input1: '',
+      input2: '',
+
+      options: [{
+          value: '选项1',
+          label: '普通会员'
+        }, {
+          value: '选项2',
+          label: '白银会员'
+        }, {
+          value: '选项3',
+          label: '黄金会员'
+        }, {
+          value: '选项4',
+          label: '钻石会员'
+        }, {
+          value: '选项5',
+          label: '至尊黑卡'
+        }],
+        value: '',
+
+    userList: [
+    {id: 'cph',sex: '男',age: 23,phonenum: '19143509817'},
+    {id: 'phc',sex: '男',age: 30,phonenum: '19999999999'},
+    {id: 'hpc',sex: '男',age: 18,phonenum: '19888888888'}
+  ]
     };
   },
   computed: {},
@@ -28,9 +142,28 @@ export default {
   beforeDestroy() {},
   destroyed() {},
   errorCaptured() {},
-  methods: {}
+  methods: {
+    click1: function(){
+          console.log('我是父级div');
+        },
+        click2: function(){
+          console.log('我是子级div');
+        }
   }
+  }
+
+
 </script>
 
 <style scoped>
+.pink{color: pink;font-size: 10px;}
+.active{background-color: pink;}
+.red{color: red;}
+
+.demo-input-size .el-input{width: 180px;margin-right: 15px;}
+
+.el-select{margin-top: 25px;width: 210px;}
+
+/* .el-row{margin-left: 20px;} */
+.btn1{margin-left: 15px;}
 </style>
